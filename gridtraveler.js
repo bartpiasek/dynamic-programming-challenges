ec1504b6de186550cde882480fa5e3589950cc38
Bart Piasek <bartpiasek@icloud.com>
2020-12-10 19:24:36 +0100

    DONE: gridtraveler memoization

--- gridtraveler.js
+++ gridtraveler.js
@@ -4,11 +4,19 @@
 
 // In how many ways can you travel to the goal on a grid with
 // dimensions m*n?
-const gridTraveler = (r ,c) => {
+const gridTraveler = (r ,c, memo={}) => {
+
+    const key = r + ',' + c;
+    if (key in memo) return memo[key];
+
     if (r === 1 && c === 1) return 1;
     if (r === 0 || c === 0) return 0;
-    return gridTraveler(r - 1, c) + gridTraveler(r, c - 1);
+
+    memo[key] = gridTraveler(r - 1, c, memo) + gridTraveler(r, c - 1, memo);
+    return memo[key];
 };
+
+
 
  console.log(gridTraveler(1,1));
  console.log(gridTraveler(2,3));
