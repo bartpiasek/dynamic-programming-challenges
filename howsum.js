6dd9296c372399e08eead96adce88e2f74f3b6cf
Bart Piasek <bartpiasek@icloud.com>
2020-12-10 21:43:03 +0100

    howsum memoization

--- howsum.js
+++ howsum.js
@@ -7,17 +7,20 @@
 
 // if there are multiple combinations possible, you may return any single one.
 
-const howSum = (targetSum, numbers) => {
+const howSum = (targetSum, numbers, memo={}) => {
+    if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return [];
     if (targetSum < 0) return null;
 
     for (let num of numbers) {
         const remainder = targetSum - num;
-        const remainderResult = howSum(remainder, numbers);
+        const remainderResult = howSum(remainder, numbers, memo);
         if (remainderResult !== null) {
-            return [ ...remainderResult, num ];
+            memo[targetSum] = [ ...remainderResult, num ];
+            return memo[targetSum];
         }
     }
+    memo[targetSum] = null;
     return null;
 };
 
