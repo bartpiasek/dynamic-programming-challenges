b615963337f44fa95e4ba9fb1f57ec232dd9f0cf
Bart Piasek <bartpiasek@icloud.com>
2020-12-18 21:22:29 +0100

    bestsum conditions in ifs

---
+++ bestsum.js
@@ -0,0 +1,35 @@
+// Write a function bestSUm(targetSum numbers) that takes in a 
+// targetSum and an array of numbers as arguments.
+
+// The function should return an array containing the shortest
+// combinations of numbers that add up to exactly the argetSum
+
+// PL: najkrótsza kombinacja liczb dająca targetSum
+
+const bestSum = (targetSum, numbers) => {
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    let shortestCombination = null;
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        const remainderCombination = bestSum(remainder, numbers);
+        if (remainderCombination !== null) {
+            const combination = [ ...remainderCombination, num ];
+            // shortest combination
+            // if the combination is shorter than te correct "shortest", update it
+            if (shortestCombination === null || combination.length < shortestCombination.length) {
+                shortestCombination = combination;
+
+            }
+            
+        }
+    }
+    return shortestCombination;
+};
+
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(6, [2, 3, 5]));
+console.log(bestSum(8, [1, 4, 5]));
+console.log(bestSum(100, [1, 2, 5, 25]));
