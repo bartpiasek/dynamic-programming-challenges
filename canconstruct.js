e61dd47374f97cd2a22ff062fb4e67ba3ec69f6c
Bart Piasek <bartpiasek@icloud.com>
2021-01-06 13:12:00 +0100

    memoization canconstruct

--- canconstruct.js
+++ canconstruct.js
@@ -6,7 +6,8 @@
 
 // You ma reuse elements of 'wordBank' as many times as needed.
 
-const canConstruct = (target, wordBank) => {
+const canConstruct = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
   if (target === "") {
     return true;
   }
@@ -14,11 +15,13 @@ const canConstruct = (target, wordBank) => {
   for (let word of wordBank) {
     if (target.indexOf(word) === 0) {
       const suffix = target.slice(word.length);
-      if (canConstruct(suffix, wordBank)) {
+      if (canConstruct(suffix, wordBank, memo)) {
+        memo[target] = true;
         return true;
       }
     }
   }
+  memo[target] = false;
   return false;
 };
 
